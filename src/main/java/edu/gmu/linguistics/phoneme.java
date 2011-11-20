122e74bad30f055aaffd6d7e37630a8d64453782
S. Kunath <skunath@gmail.com>
2011-11-20 16:22:14 -0500

    changes for javadoc

--- src/main/java/edu/gmu/linguistics/phoneme.java
+++ src/main/java/edu/gmu/linguistics/phoneme.java
@@ -1,3 +1,9 @@
+/**
+ * @author      S Kunath <skunath@gmail.comm>
+ * @version    	1.0                   
+ * @since       2010-03-31
+ */
+
 package edu.gmu.linguistics;
 
 public class phoneme {
--- src/main/java/edu/gmu/linguistics/phono.java
+++ src/main/java/edu/gmu/linguistics/phono.java
@@ -1,3 +1,9 @@
+/**
+ * @author      S Kunath <skunath@gmail.comm>
+ * @version    	1.0                   
+ * @since       2010-03-31
+ */
+
 package edu.gmu.linguistics;
 import java.io.*;
 import java.util.*;
